04d5c226770b5194ec955748181d798bf73d6a01
rifatrakib <Rakib.1508@outlook.com>
2022-08-29 19:36:17 +0600

    manipulate data sent by json as pydantic model object

--- body-param.py
+++ body-param.py
@@ -15,4 +15,8 @@ class Item(BaseModel):
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
