8b2a3cb7dd0da981601fd458f591e166b215c997
rifatrakib <Rakib.1508@outlook.com>
2022-08-29 19:56:15 +0600

    add additional metadata to be used in openAPI

--- str-validation.py
+++ str-validation.py
@@ -8,7 +8,10 @@ app = FastAPI()
 @app.get("/items/")
 async def read_items(
     q: List[str] = Query(
-        default=["foo", "bar"], min_length=3, max_length=50, regex="^fixedquery$"
+        default=["foo", "bar"], min_length=3, max_length=50, regex="^fixedquery$",
+        alias="item-query", title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        deprecated=True
     )
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
