7e788feb22960ff6622f107089fd5caae88dcf59
rifatrakib <Rakib.1508@outlook.com>
2022-08-29 19:57:55 +0600

    add hidden parameter that will not show in openAPI

--- str-validation.py
+++ str-validation.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from fastapi import FastAPI, Query
 from pydantic import Required
 
@@ -12,9 +12,12 @@ async def read_items(
         alias="item-query", title="Query string",
         description="Query string for the items to search in the database that have a good match",
         deprecated=True
-    )
+    ),
+    hidden_query: Union[str, None] = Query(default=None, include_in_schema=False)
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
+    if hidden_query:
+        results.update({"hidden_query": hidden_query})
     return results
