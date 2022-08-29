80e8458b4a8c48475c376b1134adee77c1858434
rifatrakib <Rakib.1508@outlook.com>
2022-08-29 19:52:05 +0600

    make required with pydantic.Required

--- str-validation.py
+++ str-validation.py
@@ -1,13 +1,13 @@
-from typing import Union
 from fastapi import FastAPI, Query
+from pydantic import Required
 
 app = FastAPI()
 
 
 @app.get("/items/")
 async def read_items(
-    q: Union[str, None] = Query(
-        default=..., min_length=3, max_length=50, regex="^fixedquery$"
+    q: str = Query(
+        default=Required, min_length=3, max_length=50, regex="^fixedquery$"
     )
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
