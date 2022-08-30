41b482f7dab410b64f81c3f7f8563f40ae44e0b1
rifatrakib <Rakib.1508@outlook.com>
2022-08-30 07:12:27 +0600

    add example request body with meta-class and inline

---
+++ request-example.py
@@ -0,0 +1,41 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+
+
+class InlineItem(BaseModel):
+    name: str = Field(example="Foo")
+    description: Union[str, None] = Field(default=None, example="A very nice Item")
+    price: float = Field(example=35.4)
+    tax: Union[float, None] = Field(default=None, example=3.2)
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.put("/inline-items/{item_id}")
+async def update_item(item_id: int, item: InlineItem):
+    results = {"item_id": item_id, "item": item}
+    return results
