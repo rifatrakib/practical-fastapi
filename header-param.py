6d0222889807fe11d253e5529c3344fbabade09c
rifatrakib <Rakib.1508@outlook.com>
2022-08-30 16:41:24 +0600

    read duplicate headers as list

--- header-param.py
+++ header-param.py
@@ -1,16 +1,23 @@
-from typing import Union
+from typing import Union, List
 from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 
 @app.get("/items/")
-def read_items(user_agent: Union[str, None] = Header(default=None)):
+async def read_items(user_agent: Union[str, None] = Header(default=None)):
     return {"User-Agent": user_agent}
 
 
 @app.get("/strange-items/")
-def read_strange_items(
+async def read_strange_items(
     strange_header: Union[str, None] = Header(default=None, convert_underscores=False)
 ):
     return {"strange_header": strange_header}
+
+
+@app.get("/duplicate-header-items")
+async def duplicate_header_items(
+    x_token: Union[List[str], None] = Header(default=None)
+):
+    return {"X-Token values": x_token}
