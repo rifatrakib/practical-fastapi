82a22ff325991568764b8ae043ee04aaee17bb7c
rifatrakib <Rakib.1508@outlook.com>
2022-08-30 16:50:32 +0600

    use different models for request and response

--- response-model.py
+++ response-model.py
@@ -1,6 +1,6 @@
 from typing import Union, List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
@@ -13,6 +13,24 @@ class Item(BaseModel):
     tags: List[str] = []
 
 
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item):
     return item
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user
