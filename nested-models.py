d9c98ee3a292ca041466e9f222a511adc18c16f7
rifatrakib <Rakib.1508@outlook.com>
2022-08-30 07:06:41 +0600

    arbitrarily nested models with complex fields

--- nested-models.py
+++ nested-models.py
@@ -19,7 +19,19 @@ class Item(BaseModel):
     images: Union[List[Image], None] = None
 
 
+class Offer(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    items: List[Item]
+
+
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
