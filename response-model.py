8aed806f26e53d8a324ffeda887e2580ebc75092
rifatrakib <Rakib.1508@outlook.com>
2022-08-30 16:57:12 +0600

    use parameters to customize response model output

--- response-model.py
+++ response-model.py
@@ -3,13 +3,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
 
 
 class Item(BaseModel):
     name: str
     description: Union[str, None] = None
     price: float
-    tax: Union[float, None] = None
+    tax: float = 10.5
     tags: List[str] = []
 
 
@@ -24,6 +29,20 @@ class UserOut(BaseModel):
     username: str
     email: EmailStr
     full_name: Union[str, None] = None
+
+
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.get(
+    "/items-partial/{item_id}",
+    response_model=Item,
+    response_model_include={"name", "description"}
+)
+async def read_partial_item(item_id: str):
+    return items[item_id]
 
 
 @app.post("/items/", response_model=Item)
