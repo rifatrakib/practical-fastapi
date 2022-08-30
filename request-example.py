b6d84a3bc7d9aafa521d6b316dc8156d8c1ce937
rifatrakib <Rakib.1508@outlook.com>
2022-08-30 07:18:26 +0600

    add multiple named examples in the path operator

--- request-example.py
+++ request-example.py
@@ -62,3 +62,41 @@ async def update_item_body(
 ):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+@app.put("/multi-body-items/{item_id}")
+async def update_item_multiple(
+    *,
+    item_id: int,
+    item: BodyItem = Body(
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
+        },
+    ),
+):
+    results = {"item_id": item_id, "item": item}
+    return results
