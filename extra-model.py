12c0238ccea817bc24cf2ecd620245adcef6c50a
rifatrakib <Rakib.1508@outlook.com>
2022-08-30 17:30:08 +0600

    use union of multiple models as response model

--- extra-model.py
+++ extra-model.py
@@ -3,6 +3,29 @@ from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
+items = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
+
+
+class BaseItem(BaseModel):
+    description: str
+    type: str
+
+
+class CarItem(BaseItem):
+    type = "car"
+
+
+class PlaneItem(BaseItem):
+    type = "plane"
+    size: int
+
 
 class UserBase(BaseModel):
     username: str
@@ -37,3 +60,8 @@ def fake_save_user(user_in: UserIn):
 async def create_user(user_in: UserIn):
     user_saved = fake_save_user(user_in)
     return user_saved
+
+
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+    return items[item_id]
