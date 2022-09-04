acd399010d245814ce0951df8509c7c787a23d0f
rifatrakib <Rakib.1508@outlook.com>
2022-09-04 14:49:06 +0600

    define dependency to generate user data from token string

--- basic/security/first-steps.py
+++ basic/security/first-steps.py
@@ -1,8 +1,35 @@
+from typing import Union
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+def fake_decode_token(token):
+    return User(
+        username=f"{token}fakedecoded",
+        email="john@example.com",
+        full_name="John Doe",
+    )
+
+
+async def get_current_user(token: str  = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me/")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
 
 
 @app.get("/items/")
