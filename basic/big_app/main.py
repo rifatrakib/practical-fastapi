6a8cb13758266f367607e863eb698d8c2430caf3
rifatrakib <Rakib.1508@outlook.com>
2022-09-06 12:11:45 +0600

    add admin app and include it in main app

---
+++ basic/big_app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Depends
+from .dependencies import get_query_token, get_token_header
+from .internal import admin
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+
+app.include_router(
+    admin.router, prefix="/admin", tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
