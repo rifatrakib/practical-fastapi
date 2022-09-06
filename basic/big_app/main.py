70c1c050e777235f1452e1c72361c105fbd4d7b7
rifatrakib <Rakib.1508@outlook.com>
2022-09-06 12:13:11 +0600

    add items app and include it with main app

--- basic/big_app/main.py
+++ basic/big_app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
-from .routers import users
+from .routers import users, items
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
 
 
 app.include_router(users.router)
+app.include_router(items.router)
 
 app.include_router(
     admin.router, prefix="/admin", tags=["admin"],
