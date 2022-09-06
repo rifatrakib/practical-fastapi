9b2ca95509ef91a7cb77ba207545c0220ee36522
rifatrakib <Rakib.1508@outlook.com>
2022-09-07 05:56:04 +0600

    customize metadata to be used in OpenAPI schema

---
+++ basic/metadata.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title="ChimichangApp",
+    description=description,
+    version="0.0.1",
+    terms_of_services="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+    docs_url="/documentation",
+    redoc_url="/",
+)
+
+
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"name": "samurai"}]
+
+
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "katana"}]
