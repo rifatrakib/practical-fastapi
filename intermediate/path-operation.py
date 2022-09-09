924b72a9c658bdfa332415aa63038f1ed24e72b1
rifatrakib <Rakib.1508@outlook.com>
2022-09-09 18:54:05 +0600

    use function to generate path operation function name as operationId

--- intermediate/path-operation.py
+++ intermediate/path-operation.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 
 app = FastAPI()
 
@@ -6,3 +7,18 @@ app = FastAPI()
 @app.get("/items/", operation_id="unique_items_operation_id")
 async def read_items():
     return [{"item_id": "Foo"}]
+
+
+def route_names_as_operation_id(app: FastAPI) -> None:
+    """
+    Simplify operation IDs so that generated API clients have simpler function names.
+    Should be called only after all routes have been added.
+    If you do this, you have to make sure each one of your path operation functions has a unique name.
+    Even if they are in different modules (Python files).
+    """
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = route.name
+
+
+route_names_as_operation_id(app)
