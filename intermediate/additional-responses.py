6611dde6bdd314651b03bd9eee742d59bc9ba876
rifatrakib <Rakib.1508@outlook.com>
2022-09-10 16:14:13 +0600

    combine information for OpenAPI schema from multiple sources

--- intermediate/additional-responses.py
+++ intermediate/additional-responses.py
@@ -18,12 +18,23 @@ class Message(BaseModel):
 @app.get(
     "/items/{item_id}",
     response_model=Item,
-    responses={404: {"model": Message}}
+    responses={
+        404: {"model": Message, "description": "The item was not found"},
+        200: {
+            "description": "Item requested by ID",
+            "content": {
+                "application/json": {
+                    "example": {"id": "bar", "value": "The bar tenders"}
+                }
+            },
+        },
+    },
 )
 async def read_item(item_id: str):
     if item_id == "foo":
         return {"id": "foo", "value": "there goes my hero"}
-    return JSONResponse(status_code=404, content={"message": "Item not found"})
+    else:
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
 
 
 @app.get(
