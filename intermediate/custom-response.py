98d673935eab76ad4268ee7df3c47f2decea18c6
rifatrakib <Rakib.1508@outlook.com>
2022-09-10 15:56:00 +0600

    return response using base Response class with media type

--- intermediate/custom-response.py
+++ intermediate/custom-response.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -18,3 +18,19 @@ class Item(BaseModel):
 def update_item(id: str, item: Item):
     json_compatible_item_data = jsonable_encoder(item)
     return JSONResponse(content=json_compatible_item_data)
+
+
+@app.get("/legacy/")
+def get_legacy_data():
+    data = """
+    <?xml version="1.0"?>
+    <shampoo>
+    <Header>
+        Apply shampoo here.
+    </Header>
+    <Body>
+        You'll have to use soap here.
+    </Body>
+    </shampoo>
+    """
+    return Response(content=data, media_type="application/xml")
