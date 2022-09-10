f69fd7597cdabd484ca05da2cd746cb1576de4e8
rifatrakib <Rakib.1508@outlook.com>
2022-09-10 16:13:22 +0600

    additional media types for the main response

--- intermediate/additional-responses.py
+++ intermediate/additional-responses.py
@@ -1,6 +1,7 @@
+from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, FileResponse
 
 app = FastAPI()
 
@@ -23,3 +24,20 @@ async def read_item(item_id: str):
     if item_id == "foo":
         return {"id": "foo", "value": "there goes my hero"}
     return JSONResponse(status_code=404, content={"message": "Item not found"})
+
+
+@app.get(
+    "/media-items/{item_id}",
+    response_model=Item,
+    responses={
+        200: {
+            "content": {"image/png": {}},
+            "description": "Return the JSON item or an image.",
+        }
+    },
+)
+async def read_media_item(item_id: str, img: Union[bool, None] = None):
+    if img:
+        return FileResponse("image.png", media_type="image/png")
+    else:
+        return {"id": "foo", "value": "there goes my hero"}
