a327a0f9e009c67c851298c0c5130bd0021be7b7
rifatrakib <Rakib.1508@outlook.com>
2022-09-10 15:54:51 +0600

    using the jsonable_encoder in a Response

---
+++ intermediate/custom-response.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from typing import Union
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: Union[str, None] = None
+
+
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
