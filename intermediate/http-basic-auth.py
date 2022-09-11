9f1d14d1aa981240e3618d4b45bcc94ce15ff378
rifatrakib <Rakib.1508@outlook.com>
2022-09-11 13:23:17 +0600

    implement basic http auth on server

---
+++ intermediate/http-basic-auth.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+app = FastAPI()
+security = HTTPBasic()
+
+
+@app.get("/users/me/")
+def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    return {"username": credentials.username, "password": credentials.password}
