d341f4004f5053c1f68174e0b76874e0f7e8352f
rifatrakib <Rakib.1508@outlook.com>
2022-09-12 11:03:20 +0600

    enforces that all incoming requests have a correctly set Host header

--- intermediate/middleware.py
+++ intermediate/middleware.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
 
 app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(TrustedHostMiddleware, allowed_host=["example.com", "*.example.com"])
 
 
 @app.get("/")
