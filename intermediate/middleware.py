803ed96804c362030b4a00f710460bf98f53f273
rifatrakib <Rakib.1508@outlook.com>
2022-09-12 11:03:58 +0600

    handles GZip responses for request that includes it in the Accept-Encoding header

--- intermediate/middleware.py
+++ intermediate/middleware.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
 
 app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(TrustedHostMiddleware, allowed_host=["example.com", "*.example.com"])
 
 
