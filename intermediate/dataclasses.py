29d8805e0d67b3dc0e5b48fa2a31340d4440ba71
rifatrakib <Rakib.1508@outlook.com>
2022-09-12 10:42:18 +0600

    use dataclasses as response models

--- intermediate/dataclasses.py
+++ intermediate/dataclasses.py
@@ -1,6 +1,6 @@
-from typing import Union
-from dataclasses import dataclass
 from fastapi import FastAPI
+from typing import Union, List
+from dataclasses import dataclass, field
 
 app = FastAPI()
 
@@ -9,6 +9,7 @@ app = FastAPI()
 class Item:
     name: str
     price: float
+    tags: List[str] = field(default_factory=list)
     description: Union[str, None] = None
     tax: Union[float, None] = None
 
@@ -16,3 +17,13 @@ class Item:
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+
+
+@app.get("/items/next/", response_model=Item)
+async def read_next_item():
+    return {
+        "name": "Island In The Moon",
+        "price": 12.99,
+        "description": "A place to be be playin' and havin' fun",
+        "tags": ["breater"],
+    }
