090aefb573d653ae1e8dc342d7f8c577cb80c1cd
rifatrakib <Rakib.1508@outlook.com>
2022-09-12 10:45:19 +0600

    replace dataclass models with pydantic dataclass submodule

--- intermediate/dataclasses.py
+++ intermediate/dataclasses.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from typing import Union, List
-from dataclasses import dataclass, field
+from dataclasses import field
+from pydantic.dataclasses import dataclass
 
 app = FastAPI()
 
 
 @dataclass
-class Item:
+class ItemBase:
+    name: str
+    description: Union[str, None] = None
+
+
+@dataclass
+class Author:
+    name: str
+    items: List[ItemBase] = field(default_factory=list)
+
+
+@dataclass
+class ItemExtended:
     name: str
     price: float
     tags: List[str] = field(default_factory=list)
@@ -15,11 +28,11 @@ class Item:
 
 
 @app.post("/items/")
-async def create_item(item: Item):
+async def create_item(item: ItemExtended):
     return item
 
 
-@app.get("/items/next/", response_model=Item)
+@app.get("/items/next/", response_model=ItemExtended)
 async def read_next_item():
     return {
         "name": "Island In The Moon",
@@ -27,3 +40,38 @@ async def read_next_item():
         "description": "A place to be be playin' and havin' fun",
         "tags": ["breater"],
     }
+
+
+@app.post("/authors/{author_id}/items/", response_model=Author)
+async def create_author_items(author_id: str, items: List[ItemBase]):
+    return {"name": author_id, "items": items}
+
+
+@app.get("/author/", response_model=List[Author])
+async def get_authors():
+    return [
+        {
+            "name": "Breaters",
+            "items": [
+                {
+                    "name": "Island In The Moon",
+                    "description": "A place to be be playin' and havin' fun",
+                },
+                {"name": "Holy Buddies"},
+            ],
+        },
+        {
+            "name": "System of an Up",
+            "items": [
+                {
+                    "name": "Salt",
+                    "description": "The kombucha mushroom people's favorite",
+                },
+                {"name": "Pad Thai"},
+                {
+                    "name": "Lonely Night",
+                    "description": "The mostests lonliest nightiest of allest",
+                },
+            ],
+        },
+    ]
