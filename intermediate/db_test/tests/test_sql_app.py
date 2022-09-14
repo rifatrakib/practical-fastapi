c804f473601a59f6c10db636e85d902fa656042d
rifatrakib <Rakib.1508@outlook.com>
2022-09-14 12:44:17 +0600

    bug fix in test function to check db connection

--- intermediate/db_test/tests/test_sql_app.py
+++ intermediate/db_test/tests/test_sql_app.py
@@ -30,14 +30,14 @@ def test_create_user():
         json={"email": "deadpool@example.com", "password": "chimichangas4life"},
     )
     
-    assert response.status_code == 200
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data["email"] == "deadpool@example.com"
     assert "id" in data
     user_id = data["id"]
     
     response = client.get(f"/users/{user_id}")
-    assert response.status_code == 200
+    assert response.status_code == 200, response.text
     data = response.json()
     assert data["email"] == "deadpool@example.com"
     assert data["id"] == user_id
